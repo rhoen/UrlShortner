cbe008bd67a5911e568c41ecca46f4ba45163ac4
rhoen+liann <rhoen+liann@appacademy.io>
2015-04-08 15:13:35 -0400

    Add random code generator to ShortenedUrl class.

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -2,4 +2,16 @@ class ShortenedUrl < ActiveRecord::Base
   validates :short_url, uniqueness: true, presence: true
   validates :submitter_id, presence: true
   validates :long_url, presence: true
+
+  def self.random_code
+    code = SecureRandom::urlsafe_base64
+
+    until !ShortenedUrl.exists?(short_url: "code")
+      code = SecureRandom::urlsafe_base64
+    end
+
+    code
+  end
+
+
 end
